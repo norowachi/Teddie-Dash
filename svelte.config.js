efe19e87bf96d0443a3de63fbf5b0c26feaefa20
norowachi <contact@noro.cc>
2025-06-06 01:45:08 +0200

    feat: docker

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
-import { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
