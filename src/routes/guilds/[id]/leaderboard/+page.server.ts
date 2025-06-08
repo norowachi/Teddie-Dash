9b441a418ff03c70db078b92ea8b0c36d81a1365
Noro <contact@noro.cc>
2025-06-08 22:12:17 +0200

    feat: refactor leaderboard loading logic to handle invite links and improve error handling

--- src/routes/guilds/[id]/leaderboard/+page.server.ts
+++ src/routes/guilds/[id]/leaderboard/+page.server.ts
@@ -4,27 +4,44 @@ import { PRIVATE_API_KEY } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
   // TODO
-  const guildId = params.id;
-  const data = await fetch(`https://api.noro.cc/757135707327299604/${guildId}/leaderboard`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: PRIVATE_API_KEY
-    }
-  });
+  let guildId = params.id;
 
-  if (data) {
-    return (await data.json()) as {
-      users: Array<{
-        id: string;
-        username: string;
-        avatar: string;
-        xp: number;
-        level: number;
-        hidden: boolean;
-      }>;
-    };
+  // check if guild is an invite link if its not a number
+  if (isNaN(Number(guildId))) {
+    const result: { guild_id: string } | undefined = await (
+      await fetch(`https://discord.com/api/v10/invites/${guildId}`).catch(console.error)
+    )
+      ?.json()
+      .catch(console.error);
+    // If the invite is not found, result will be undefined
+    if (!result || !result.guild_id) error(404, 'Not found');
+
+    guildId = result.guild_id;
   }
 
-  error(404, 'Not found');
+  const data: {
+    users: Array<{
+      id: string;
+      username: string;
+      avatar: string;
+      xp: number;
+      level: number;
+      hidden: boolean;
+    }>;
+  } = await (
+    await fetch(`https://api.noro.cc/757135707327299604/${guildId}/leaderboard`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: PRIVATE_API_KEY
+      }
+    }).catch(console.error)
+  )
+    ?.json()
+    .catch(console.error);
+
+  // If the data is not found, return error
+  if (!data) error(404, 'Not found');
+
+  return data;
 };
